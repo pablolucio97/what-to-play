e00646cbc8ff4d7e8855ebee54a46ad57f7bd10b
pablolucio97 <pablolucio97@users.noreply.github.com>
2021-11-22 12:47:31 -0300

    styled components configs

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+
+import {GlobalStyles} from '../styles/GlobalStyle'
+
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyles/>
+      <Component {...pageProps}/>
+    </>
+  )
+}
+
+export default MyApp
---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+     *{
+        font-family: 'Oswald', sans-serif;
+        letter-spacing: .04rem;
+    }
+
+    body{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        overflow-x: hidden;
+    }
+
+    a{
+        text-decoration-line: none;
+    }
+`;
