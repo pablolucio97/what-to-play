88c094a94a9ec5a7f9241427aca51151f8469ec5
pablolucio97 <pablolucio97@users.noreply.github.com>
2021-11-29 08:36:00 -0300

    Implemented function to add the user to db at authentication

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -1,8 +1,10 @@
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers'
+import { query as q } from 'faunadb'
+import { fauna } from '../../../services/faunadb'
 
 export default NextAuth({
-    providers:[
+    providers: [
         Providers.GitHub({
             clientId: process.env.GITHUB_CLIENT_ID,
             clientSecret: process.env.GITHUB_CLIENT_SECRET,
@@ -16,5 +18,28 @@ export default NextAuth({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
         })
-    ]
+    ],
+    callbacks: {
+        async signIn(user) {
+            const { email, name } = user;
+           try {
+            await fauna.query(
+                q.If(
+                  q.Not(
+                    q.Exists(
+                      q.Match(q.Index("get_users"), q.Casefold(user.email))
+                    )
+                  ),
+                  q.Create(q.Collection("users"), { data: { email, name } }),
+                  q.Get(q.Match(q.Index("get_users"), q.Casefold(user.email)))
+                )
+              );
+
+            return true
+           } catch (error) {
+               console.log(error)
+               return false
+           }
+        }
+    }
 })
